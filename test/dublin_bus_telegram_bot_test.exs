bce72aa6a6095b07e54e8cc8426c17a5b3fc8d7d
Carlo Colombo <carlo.colombo@gmx.com>
2016-05-18 07:14:08 +0100

    some test

--- test/dublin_bus_telegram_bot_test.exs
+++ test/dublin_bus_telegram_bot_test.exs
@@ -2,7 +2,10 @@ defmodule DublinBusTelegramBotTest do
   use ExUnit.Case
   doctest DublinBusTelegramBot
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "decorating a function" do
+    defmeter a_func(arg1, arg2, arg3) do
+      IO.puts("do nothing")
+    end
   end
 end
+#
