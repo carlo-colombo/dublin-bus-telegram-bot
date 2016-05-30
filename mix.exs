acf02c069297beef6252c16532931c78d5b425ad
Carlo Colombo <carlo.colombo@gmx.com>
2016-05-30 19:41:34 +0100

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule DublinBusTelegramBot.Mixfile do
 
   def project do
     [app: :dublin_bus_telegram_bot,
-     version: "0.4.5",
+     version: "0.4.6",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
